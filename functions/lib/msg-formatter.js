2a78fd2ad994c2f701a1f24a97295be166667a26
matan-ubuntu <matanby94@gmail.com>
2019-02-12 19:47:32 +0200

    ..

--- functions/lib/msg-formatter.js
+++ functions/lib/msg-formatter.js
@@ -4,39 +4,39 @@ const formatMessage = (err, messages) => {
 
 module.exports = {
     kickedOffSuccess: (featureCode) => `
-    Ok, your new branch is ready
-    You can go check it out here: http://osherel.net.s3-website-us-east-1.amazonaws.com/${featureCode}`,
+Ok, your new branch is ready
+You can go check it out here: http://osherel.net.s3-website-us-east-1.amazonaws.com/${featureCode}`,
     kickedOffFailed: (featureCode, err) => formatMessage(err, {
         EGITBRANCHEXISTS: `
-            Oops! it seems the branch name (${featureCode}) is taken, please choose another name or remove the existing branch by typing: "reject ${featureCode}"        
+Oops! it seems the branch name (${featureCode}) is taken, please choose another name or remove the existing branch by typing: "reject ${featureCode}"        
         `
     }),
     acceptSuccess: (featureCode) => `
-        We did it! the branch ${ featureCode } is ready.
-        Go check it out here: http://osherel.net.s3-website-us-east-1.amazonaws.com/${ featureCode }    
+We did it! the branch ${ featureCode } is ready.
+Go check it out here: http://osherel.net.s3-website-us-east-1.amazonaws.com/${ featureCode }    
     `,
     acceptFailed: (featureCode, err) => formatMessage(err, {
         EGITBRANCHNOTFOUND: `
-            Oops! We were unable to find the project you were looking for.
-            Please check if ${featureCode} exists or create it by writing: "lets kickoff ${featureCode}"
+Oops! We were unable to find the project you were looking for.
+Please check if ${featureCode} exists or create it by writing: "lets kickoff ${featureCode}"
         `
     }),
     rejectSuccess: (featureCode) => `
-        Ok, The branch ${ featureCode } has been removed.    
+Ok, The branch ${ featureCode } has been removed.    
     `,
     rejectFailed: (featureCode, err) => formatMessage(err, {
         EGITBRANCHNOTFOUND: `
-            Oops! We were unable to find the project you were looking for.
-            Please check if ${featureCode} exists or create it by writing: "lets kickoff ${featureCode}"
+Oops! We were unable to find the project you were looking for.
+Please check if ${featureCode} exists or create it by writing: "lets kickoff ${featureCode}"
         `,
     }),
     toMasterSuccess: (featureCode) => `
-        Ok, The branch ${ featureCode } has been pushed to master.    
+Ok, The branch ${ featureCode } has been pushed to master.    
     `,
     toMasterFailed: (featureCode, err) => formatMessage(err, {
         EGITBRANCHNOTFOUND: `
-            Oops! We were unable to find the project you were looking for.
-            Please check if ${featureCode} exists or create it by writing: "lets kickoff ${featureCode}"
+Oops! We were unable to find the project you were looking for.
+Please check if ${featureCode} exists or create it by writing: "lets kickoff ${featureCode}"
         `,
     }),
 }
