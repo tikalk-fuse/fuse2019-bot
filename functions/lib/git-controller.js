b220a999fcc91bee8cf5a38fc3e098325cd5eeac
Osher E <osher.filter@gmail.com>
2019-02-12 15:04:13 +0200

    kickoff worked w/wo git repo dir

--- functions/lib/git-controller.js
+++ functions/lib/git-controller.js
@@ -1,48 +1,56 @@
 const git = require('simple-git/promise');
 
-const USER = 'fuse2019team4';
-const PASS = 'fuse3#t3am4';
-const REPO = 'github.com/tikalk-fuse/fuse-2019-the-product.git';
-
-const remote = `https://${USER}:${encodeURIComponent(PASS)}@${REPO}`;
-
 const param = process.argv[2];
 console.log(param);
 
 
-const obtainRepo = async () => {
+const obtainRepo = async (remote, dir) => {
     try {
         console.info('starting to work');
         const clone = await git().silent(true).clone(remote);
+        console.info('cloned');
         return clone;
     } catch(err) {
-            const _err = new Error([err]);
-            if (_err.message.includes('already exists')) return git().pull();
-            return err;
+        const _err = new Error([err]);
+        console.log('clone error:: ',  err.stack);
+        if (_err.message.match(/destination path '.*' already exists and is not an empty directory/)) {
+            console.log('already cloned. pulling...');
+            return git(dir).pull('origin', 'stage')
+        };
+        return err;
     }
 }
 
 class GitBot {
 
     constructor(config) {
+        const {
+          user,
+          password,
+          repo
+        } = config;
         this._config = config;
+        this.dir = /\/([^.\/]*)\.git/.exec(repo)[1];console.log('dir', this.dir)
+        this.remote = repo.replace('https://', `https://${encodeURIComponent(user)}:${encodeURIComponent(password)}@`);
     }
     
     // git checkout
-    async kickoff({featureCode}) {
+    async kickoff({branch}) {
         try {
-        await obtainRepo();
-        console.log('checking out ' , featureCode);
-        await git().raw(['checkout', '-b', featureCode]);
+        await obtainRepo(this.remote, this.dir);
+        console.log('entering stage');
+        await git(this.dir).checkout('stage');
+        console.log('creating branch ' ,  branch );
+        await git(this.dir).checkoutLocalBranch(branch);
 
         } catch(err) {
-            console.warn(err);
+            console.warn('problem kickoff', err);
             return err;
         }
     }
 
     // don't merge
-    async eject({featureCode}) {
+    async reject({branch}) {
         try {
 
         } catch(err) {
@@ -52,7 +60,7 @@ class GitBot {
     }
 
     // merge to stage 
-    async accept({featureCode}) {
+    async accept({branch}) {
         try {
 
         } catch(err) {
@@ -62,11 +70,7 @@ class GitBot {
     }
 }
 
-const botFactory = (config) =>  new GitBot(config);
+const botFactory = (config) =>  console.log('creating', config) || new GitBot(config);
 
-botFactory({}).kickoff({featureCode: 'feature/666-yair-test-fix'})
-.then(() => {
-   console.log('success');
-})
-.catch(err => console.log(err));
+module.exports = botFactory
 
